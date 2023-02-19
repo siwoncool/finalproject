44fb8bb05f4caad92d3a827240af8a311c89881f
siwoncool <qkrtldnjs03@naver.com>
2023-02-19 21:53:40 +0900

    파이널 프로젝트 atelier

---
+++ src/main/java/com/artisans/atelier/dto/LiteraturePayDTO.java
@@ -0,0 +1,12 @@
+package com.artisans.atelier.dto;
+
+import lombok.Data;
+import org.apache.ibatis.type.Alias;
+
+@Alias("literaturepay")
+@Data
+public class LiteraturePayDTO {
+    private int litBoaCode;    //결제한 게시글
+    private int litMemCode;     // 결제하는 회원
+
+}
